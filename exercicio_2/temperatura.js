b1b1b44176c7e5cbf496979157af71bd0910c32b
Diego Leite Asprino <diegoasprino07@gmail.com>
2025-04-23 08:15:25 -0300

    adicionar exercicio 2

---
+++ exercicio_2/temperatura.js
@@ -0,0 +1,8 @@
+const readline = require("readline").createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+let conversorTemp = readline.question("");
+
+console.log(conversorTemp);
