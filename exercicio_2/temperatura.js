5372d4cb13da7719f7114ba56f88c8a942b4da36
Diego Leite Asprino <diegoasprino07@gmail.com>
2025-04-23 08:39:38 -0300

    continua medio

--- exercicio_2/temperatura.js
+++ exercicio_2/temperatura.js
@@ -3,6 +3,32 @@ const readline = require("readline").createInterface({
     output : process.stdout
 });
 
-let conversorTemp = readline.question("");
+let valor = readline.question("Você gostaria de converter celcius para fareheint ou fareheint para celcius :", (valor) => {
+    let opcao1 = "celcius para fareheint" 
+    let opcao2 =  "fareheint para celcius"
 
-console.log(conversorTemp);
+    readline.close()
+    return valor
+});
+
+
+readline.question(`coloque um valor em ${valor}, para converter :`, (temperatura) => {
+    let temperaturas = parseFloat(temperatura)
+    let conversao
+
+    if(!isNaN(temperaturas)){
+        console.error("Error, coloque um numero flutuante por favor")
+    }
+    else{
+        if(valor == opcao1){
+            // F = (C * 9/5) + 32.
+            conversao = (temperaturas * 9/5) + 32
+        }
+        else if(valor == opcao2){
+            // C = (F - 32) * 5/9
+            conversao = (temperaturas - 32) * 5/9
+        }
+    }
+    readline.close();
+})
+
