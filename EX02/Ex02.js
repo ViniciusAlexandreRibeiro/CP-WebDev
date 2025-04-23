0592be4fe2f6b813aba7c8cc5a81e03b2b0c66bf
Diego Leite Asprino <diegoasprino07@gmail.com>
2025-04-23 09:33:24 -0300

    rework do codigo

--- EX02/Ex02.js
+++ EX02/Ex02.js
@@ -6,32 +6,26 @@ const rl = readline.createInterface({
 });
 
 rl.question("Qual escala de temperatura você deseja converter? Digite:\n1 para Celsius para Fahrenheit\n2 para Fahrenheit para Celsius\n", (escala) => {
-    if (escala === "1") {
-        rl.question("Digite a temperatura em Celsius: ", (celsius) => {
-            const celsiusValue = parseFloat(celsius);
-            if (isNaN(celsiusValue)) {
+    
+        rl.question("Digite a temperatura: ", (temperatura) => {
+            const Value = parseFloat(temperatura);
+            if (isNaN(temperatura)) {
                 console.log("Por favor, insira um valor numérico válido.");
             } else {
-                const fahrenheit = (celsiusValue * 9 / 5) + 32;
-                console.log(`Temperatura original: ${celsiusValue}°C`);
-                console.log(`Temperatura convertida: ${fahrenheit.toFixed(2)}°F`);
+                let result
+
+                if(escala === "1"){
+                    result = (temperatura * 9 / 5) + 32;
+                    console.log(`Temperatura original: ${temperatura}°C`);
+                    console.log(`Temperatura convertida: ${result.toFixed(2)}°F`);
+                }
+                else if(escala === "2"){
+                    result = (temperatura - 32) * 5 / 9;
+                    console.log(`Temperatura original: ${temperatura}°F`);
+                    console.log(`Temperatura convertida: ${result.toFixed(2)}°C`);
+                }
+                
             }
             rl.close();
         });
-    } else if (escala === "2") {
-        rl.question("Digite a temperatura em Fahrenheit: ", (fahrenheit) => {
-            const fahrenheitValue = parseFloat(fahrenheit);
-            if (isNaN(fahrenheitValue)) {
-                console.log("Por favor, insira um valor numérico válido.");
-            } else {
-                const celsius = (fahrenheitValue - 32) * 5 / 9;
-                console.log(`Temperatura original: ${fahrenheitValue}°F`);
-                console.log(`Temperatura convertida: ${celsius.toFixed(2)}°C`);
-            }
-            rl.close();
-        });
-    } else {
-        console.log("Opção inválida. Por favor, escolha 1 ou 2.");
-        rl.close();
-    }
 });
